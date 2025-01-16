947c8afdc3b20afbba91f9ffa11d8cd276a4c458
caher1 <ch1212ceti@gmail.com>
2025-01-15 21:29:12 -0600

    feat: Second Design x Animation

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const cards = document.querySelectorAll('.card');
+    const people = document.querySelectorAll('.man-img, .woman-img');
+    const charts = document.querySelectorAll('.charts-container');
+
+    charts.forEach(chart => {
+        chart.classList.add('visible');
+    })
+    people.forEach(person => {
+        person.classList.add('visible');
+    });
     cards.forEach(card => {
         card.classList.add('visible');
     });
